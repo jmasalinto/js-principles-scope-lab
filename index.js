72d9c58975464ca6ea889e69d769c4e72dcadb1d
Jamie Masalinto <jmasalinto@gmail.com>
2021-04-28 18:31:59 -0400

    commit tree d28ff025518d1e3311d575458b91700643756ce4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(customerName) {
+    console.log(customerName.toUpperCase())
+}
+
+function setBestCustomer() {
+    
+}
